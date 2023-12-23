4e6aff6059f9ba2c85a1f3f76a062d75fb69ee05
= <damyoung1225@gmail.com>
2023-12-23 17:38:47 +0900

    어노테이션 만들기

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,19 @@
 package hello.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
 @Component
 @Primary
+/*
+@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy -> 가져다 쓰는 곳에서도 똑같이 어노테이션해주면 된다.
+*/
 public class RateDiscountPolicy implements DiscountPolicy {
 
 	private int discountPercent = 10;
